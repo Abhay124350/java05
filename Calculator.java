c230e8205212a2c60b2c26cd6d4be20b5a5ed860
abhay <abhaysinghtomar12435@gmail.com>
2025-08-27 09:33:52 +0530

    inheritance 27/08/25

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x,y,z=0;
+        System.out.println("Enter the value of x and y");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        try
+        {
+            z = x/y;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally
+            {
+            System.out.println(z);
+            }
+    }
+}
---
+++ Myshape.java
@@ -0,0 +1,7 @@
+public class Myshape {
+    public static void main(String[] args) {
+        shape s = new newshape();
+        s.draw(12);
+        s.draw(12, 13);
+    }
+}
